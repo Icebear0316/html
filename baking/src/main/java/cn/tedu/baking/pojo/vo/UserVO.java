dddbb28e6c7128c478d4afbd6be361f5c06b31b6
DELL <Icebear08042@163.com>
2023-06-01 09:26:59 +0800

    baking

--- baking/src/main/java/cn/tedu/baking/pojo/vo/UserVO.java
+++ baking/src/main/java/cn/tedu/baking/pojo/vo/UserVO.java
@@ -1,19 +1,13 @@
 package cn.tedu.baking.pojo.vo;
 
+import lombok.Data;
+
 import java.util.Date;
 
+
+@Data
 public class UserVO {
     private Long id;
-
-    private String nickName;
-
-    private String userName;
-
     private String password;
 
-    private Integer isAdmin;
-
-    private String imgUrl;
-
-    private Date createTime;
 }
