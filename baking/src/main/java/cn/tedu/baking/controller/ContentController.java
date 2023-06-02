7d2a6758c16a5c05b1f421ec1c5a29dc162a6842
DELL <Icebear08042@163.com>
2023-06-02 15:38:50 +0800

    ContentController

---
+++ baking/src/main/java/cn/tedu/baking/controller/ContentController.java
@@ -0,0 +1,35 @@
+package cn.tedu.baking.controller;
+
+
+import cn.tedu.baking.mapper.ContentMapper;
+import cn.tedu.baking.pojo.dto.ContentDTO;
+import cn.tedu.baking.pojo.entity.Content;
+import cn.tedu.baking.response.ResultVO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/v1/contents/")
+public class ContentController {
+    @Autowired
+    ContentMapper mapper;
+
+    /**
+     * http://localhost:8088/v1/contents/add-new
+     * @return
+     * */
+    @RequestMapping("add-new")
+    public ResultVO addNew(@RequestBody ContentDTO contentDTO){
+        System.out.println("contentDTO = " + contentDTO);
+        Content content = new Content();
+        BeanUtils.copyProperties(contentDTO,content);
+        content.setCreateTime(new Date());
+
+        mapper.insert(content);
+        return ResultVO.ok();
+    }
+}
