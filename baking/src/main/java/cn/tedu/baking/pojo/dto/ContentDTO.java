f2af40f1f913d03c941b7599541b47f2ebba6627
DELL <Icebear08042@163.com>
2023-06-02 15:40:07 +0800

    ContentDTO

---
+++ baking/src/main/java/cn/tedu/baking/pojo/dto/ContentDTO.java
@@ -0,0 +1,15 @@
+package cn.tedu.baking.pojo.dto;
+
+import lombok.Data;
+
+@Data
+public class ContentDTO {
+    private String title;
+    private String imgUrl;
+    private String videoUrl;
+    private String brief;
+    private Long type;
+    private Long categoryId;
+    private Long createBy;
+    private String content;
+}
