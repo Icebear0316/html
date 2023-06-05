7157a10497e5403c67b20a328daf03e252eb0df8
DELL <Icebear08042@163.com>
2023-06-05 11:20:23 +0800

    时间格式修改

--- baking/src/main/java/cn/tedu/baking/pojo/vo/ContentManagementVO.java
+++ baking/src/main/java/cn/tedu/baking/pojo/vo/ContentManagementVO.java
@@ -1,11 +1,13 @@
 package cn.tedu.baking.pojo.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class ContentManagementVO {
+
     private Long id;
     private String title;
     private String imgUrl;
@@ -14,5 +16,7 @@ public class ContentManagementVO {
     private String categoryName;
     private Integer viewCount;
     private Integer commentCount;
+    @JsonFormat(pattern = "yyyy年MM月dd日 HH时mm分ss秒",timezone = "GMT+8")
     private Date createTime;
+
 }
